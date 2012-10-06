dbbdc2638980e5e493926842fded19116de5d77b
Korbinian Polk <korbinianpolk@googlemail.com>
2012-10-06 13:22:28 +0200

    moved sticker to top of sidebar

--- front-page.php
+++ front-page.php
@@ -8,13 +8,15 @@
 
 <div id="aside" >
 	<div class="sidebar-front">
+		<div class="sticker">
+		<?php if ( ! dynamic_sidebar( 'sticker' ) ) : ?>
+		<?php endif; ?>
+		</div>
 		<?php if ( ! dynamic_sidebar( 'sidebar-front' ) ) : ?>
 		<aside class="widget"><img src="http://placehold.it/296x240"></aside>
 		<aside class="widget"><img src="http://placehold.it/296x120"></aside>
 		<?php endif; ?>
 
-		<?php if ( ! dynamic_sidebar( 'sticker' ) ) : ?>
-		<?php endif; ?>
 	</div>
 
 </div>
